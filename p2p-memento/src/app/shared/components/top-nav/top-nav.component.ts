8e6bf4b29cc1c84032f58c9dd835b85d5e0efabb
alvien salvador <biboy@alviens-MacBook-Pro-2.local>
2025-07-06 15:41:24 +1200

    Landing page routing to login and signup page works

--- p2p-memento/src/app/shared/components/top-nav/top-nav.component.ts
+++ p2p-memento/src/app/shared/components/top-nav/top-nav.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatButtonModule} from '@angular/material/button';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-top-nav',
-  imports: [MatToolbarModule, MatButtonModule],
+  imports: [MatToolbarModule, MatButtonModule, RouterLink],
   templateUrl: './top-nav.component.html',
   styleUrl: './top-nav.component.scss'
 })
