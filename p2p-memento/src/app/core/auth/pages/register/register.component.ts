3e69af7403e35568e5a98cec7316160f7016fe65
alvien salvador <biboy@alviens-MacBook-Pro-2.local>
2025-07-06 16:25:46 +1200

    set up register card

--- p2p-memento/src/app/core/auth/pages/register/register.component.ts
+++ p2p-memento/src/app/core/auth/pages/register/register.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [MatCardModule, MatButtonModule, MatFormFieldModule,MatInputModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
