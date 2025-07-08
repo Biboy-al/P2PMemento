6917d2ebd2a9a286c725ac86b00b22ade0a8d86a
alvien salvador <biboy@alviens-MacBook-Pro-2.local>
2025-07-08 15:49:39 +1200

    Login now works

--- p2p-memento/src/app/core/auth/pages/login/login.component.ts
+++ p2p-memento/src/app/core/auth/pages/login/login.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { LoginUserService } from '../../service/login-user.service';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [MatCardModule, MatButtonModule, MatFormFieldModule,MatInputModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+
+  private formBuilder = inject(FormBuilder);
+  private login = inject(LoginUserService);
+
+  loginForm = this.formBuilder.group({
+    email: ['', Validators.required],
+    password: ['', Validators.required]
+  });
+
+  loginUser(){
+    const email = this.loginForm.get("email")?.value!;
+    const password = this.loginForm.get("password")?.value!;
+    this.login.login(email,password);
+  };
 
 }
--- p2p-memento/src/app/core/auth/service/login-user.service.ts
+++ p2p-memento/src/app/core/auth/service/login-user.service.ts
@@ -1,14 +1,21 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
 import { auth } from '../../../../firebase/firebase';
 import { signInWithEmailAndPassword } from 'firebase/auth';
+import { Router } from '@angular/router';
 @Injectable({
   providedIn: 'root'
 })
 export class LoginUserService {
 
+  private router = inject(Router)
+
   constructor() { }
 
   login(email: string, password: string){
-    signInWithEmailAndPassword(auth, email, password);
+    signInWithEmailAndPassword(auth, email, password).then(
+      (userCred) =>{  
+        this.router.navigate(['/dashboard']);
+      }
+    ).catch();
   }
 }
